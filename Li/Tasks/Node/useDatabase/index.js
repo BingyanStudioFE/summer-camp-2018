2e85e45f17ff599425fec2432b6ccfb7104ad10c
liyinghao666 <928815137@qq.com>
2018-07-16 20:20:53 +0800

    add:express_learning

--- Li/Tasks/Node/useDatabase/index.js
+++ Li/Tasks/Node/useDatabase/index.js
@@ -1,14 +1,24 @@
-const express = require('express');
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+// const express = require('express');
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/";
  
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("rabbit");
-    var myobj = { name: "lyh" };
-    dbo.collection("site").insertOne(myobj, function(err, res) {
-        if (err) throw err;
-        console.log("你倒是连上啊");
-        db.close();
-    });
-});
+// const app = new express();
+// app.use(function(req,res,next){
+//     console.log('1');
+//     next();
+// })
+// app.use(function(req,res,next){
+//     console.log('2');
+//     res.status(200).end();
+// })
+// app.listen(3000);
+// MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("rabbit");
+//     var myobj = { name: "lyh" };
+//     dbo.collection("site").insertOne(myobj, function(err, res) {
+//         if (err) throw err;
+//         console.log("你倒是连上啊");
+//         db.close();
+//     });
+// });
