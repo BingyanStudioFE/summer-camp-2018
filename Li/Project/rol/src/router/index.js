ed19360e1f25963593d64cc1b8fb1d2498b5089e
liyinghao666 <928815137@qq.com>
2018-07-23 01:11:49 +0800

    add:projectByVue

--- Li/Project/rol/src/router/index.js
+++ Li/Project/rol/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -8,8 +7,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Welcome',
+      component: () => import('@/components/Welcome')
     }
   ]
 })
